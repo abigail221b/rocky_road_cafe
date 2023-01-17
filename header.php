583f9f97fd8fc5a643549ec9d4b2bb3f80251df8
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-17 12:58:53 -0800

    Render list of menu page links

--- header.php
+++ header.php
@@ -9,38 +9,10 @@
 
         <!-- Nav -->
             <nav id="nav">
-                <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-                <ul>
-                    <li><a href="gallery.php">Gallery</a></li>
-                    <li>
-                        <a href="menu.php">Menu</a>
-                        <ul>
-                            <li><a href="#">Lorem ipsum dolor</a></li>
-                            <li><a href="#">Magna phasellus</a></li>
-                            <li><a href="#">Etiam dolore nisl</a></li>
-                            <li>
-                                <a href="#">And a submenu &hellip;</a>
-                                <ul>
-                                    <li><a href="#">Lorem ipsum dolor</a></li>
-                                    <li><a href="#">Phasellus consequat</a></li>
-                                    <li><a href="#">Magna phasellus</a></li>
-                                    <li><a href="#">Etiam dolore nisl</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Veroeros feugiat</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="index.php"><img src="<?php echo get_bloginfo("template_directory"); ?>/images/logo.png" width="115px" /></a></li>
-                    <li><a href="blog.php">Blog</a></li>
-                    <li>
-                        <a href="about.php">About Us</a>
-                        <ul>
-                            <li><a href="about.php">Our Story</a></li>
-                            <li><a href="locations.php">Locations</a></li>
-                            <li><a href="contact.php">Contact Us</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <?php wp_nav_menu( array(
+                    "theme_location" => "header-menu",
+                    "container" => "ul"
+                ) ); ?>
             </nav>
 
     </div>
