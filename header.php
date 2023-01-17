7f96b52e531baf2525f8f0353c77ab5d110a5593
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-17 12:03:19 -0800

    Put header markup in header.php

---
+++ header.php
@@ -0,0 +1,50 @@
+<div id="header">
+
+        <!-- Inner -->
+            <div class="inner">
+                <header>
+                    <h1><a href="index.php" id="logo">Rocky Road Cafe</a></h1>
+                    <hr />
+                    <p>Another fine freebie by HTML5 UP</p>
+                </header>
+                <footer>
+                    <a href="#banner" class="button circled scrolly">Start</a>
+                </footer>
+            </div>
+
+        <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li>
+                        <a href="menu.php">Menu</a>
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Magna phasellus</a></li>
+                            <li><a href="#">Etiam dolore nisl</a></li>
+                            <li>
+                                <a href="#">And a submenu &hellip;</a>
+                                <ul>
+                                    <li><a href="#">Lorem ipsum dolor</a></li>
+                                    <li><a href="#">Phasellus consequat</a></li>
+                                    <li><a href="#">Magna phasellus</a></li>
+                                    <li><a href="#">Etiam dolore nisl</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Veroeros feugiat</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="index.php"><img src="<?php echo get_bloginfo("template_directory"); ?>/images/logo.png" width="115px" /></a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li>
+                        <a href="about.php">About Us</a>
+                        <ul>
+                            <li><a href="about.php">Our Story</a></li>
+                            <li><a href="locations.php">Locations</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+
+    </div>
