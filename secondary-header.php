6892fd080e36d25f1ca463736e6b713123573714
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-19 14:46:30 -0800

    Add brand to header Includes logo and name on the left side of header

--- secondary-header.php
+++ secondary-header.php
@@ -1,5 +1,11 @@
 <div id="secondary-header">
 
+        <!-- Cafe Name and Logo -->
+        <div id="brand">
+            <img src="<?php echo get_bloginfo("template_directory"); ?>/images/logo.png" width="75px" />
+            <h2><a href="">Rocky Road Cafe</a></h2>
+        </div>
+
         <!-- Nav -->
             <nav id="nav">
                 <?php wp_nav_menu( array(
