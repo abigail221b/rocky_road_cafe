d721331dcf5b757345ca47368132edbb4b9e506b
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-19 14:03:51 -0800

    Init secondary header Used only in blog and other pages, not on landing page

---
+++ secondary-header.php
@@ -0,0 +1,10 @@
+<div id="header">
+        <!-- Nav -->
+            <nav id="nav">
+                <?php wp_nav_menu( array(
+                    "theme_location" => "header-menu",
+                    "container" => "ul"
+                ) ); ?>
+            </nav>
+
+    </div>
