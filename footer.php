f7fc74aa43e2b1e049aec26391ed45aed10c94cf
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-26 12:55:16 -0800

    Add and rearrage footer content

--- footer.php
+++ footer.php
@@ -1,23 +1,26 @@
 <div id="footer">
     <div class="container">
+        <header>
+            <img src="<?php echo get_bloginfo("template_directory"); ?>/images/logo_light.png" width="150px" />
+            <h2>Rocky Road Cafe</h2>
+        </header>
         <div class="row">
-            <header>
-                <h2>Rocky Road Cafe</h2>
-            </header>
-        </div>
-        <div class="row">
-            <section class="">
-                <div>
-                    1234 Somewhere St.<br>
-                    City, XYZ 123
-                </div>
-                <br>
-                <div>
-                    Monday-Friday....................6am-9pm<br>
-                    Saturday & Sunday............7am-8pm
-                </div>
+            <div>
+                <h3>Location</h3><br>
+                1234 Somewhere St.<br>
+                City, XYZ 123 <br>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1301.7239315561021!2d-123.14761647619629!3d49.26791029546487!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1674765943249!5m2!1sen!2sca" width="250" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div>
+                <h3>Opening hours</h3><br>
+                Monday................................6am-9pm<br>
+                Tuesday...............................6am-9pm<br>
+                Wednesday..........................6am-9pm<br>
+                Thursday..............................6am-9pm<br>
+                Friday....................................6am-9pm<br>
+                Saturday & Sunday............7am-8pm
+            </div>
 
-            </section>
             <div class="col-12">
                 <!-- Copyright -->
                     <div class="copyright">
