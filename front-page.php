3d9861734407bd7952fb72ae9ac3cc420794668e
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-26 13:10:05 -0800

    Add slogan

--- front-page.php
+++ front-page.php
@@ -23,7 +23,7 @@
 						<header>
 							<h1><a href="index.php" id="logo">Rocky Road Cafe</a></h1>
 							<hr />
-							<p>Another fine freebie by HTML5 UP</p>
+							<p>Where every sip is a smooth ride</p>
 						</header>
 						<footer>
 							<a href="<?php get_bloginfo("template-directory"); ?>/menu" class="button">Our Menu</a>
