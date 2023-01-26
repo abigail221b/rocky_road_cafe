55e489719cfbdac496c73416c3088a0f1fddfdd1
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-25 20:12:27 -0800

    Add address

--- footer.php
+++ footer.php
@@ -7,14 +7,22 @@
         </div>
         <div class="row">
             <section class="">
-                <p>Monday-Friday....................6am-9pm</p>
-                <p>Saturday & Sunday............7am-8pm</p>
+                <div>
+                    1234 Somewhere St.<br>
+                    City, XYZ 123
+                </div>
+                <br>
+                <div>
+                    Monday-Friday....................6am-9pm<br>
+                    Saturday & Sunday............7am-8pm
+                </div>
+
             </section>
             <div class="col-12">
                 <!-- Copyright -->
                     <div class="copyright">
                         <ul class="menu">
-                            <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                            <li>Design based on: <a href="http://html5up.net">HTML5 UP</a></li>
                         </ul>
                     </div>
 
