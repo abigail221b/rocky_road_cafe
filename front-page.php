fc566d4e5917f9ab2ca39e797abf077b19dd456f
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-26 14:19:45 -0800

    Add customer testimonial section with blockquotes

--- front-page.php
+++ front-page.php
@@ -111,25 +111,16 @@
 					<article id="main" class="container special">
 						<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
 						<header>
-							<h2><a href="#">Sed massa imperdiet magnis</a></h2>
-							<p>
-								Sociis aenean eu aenean mollis mollis facilisis primis ornare penatibus aenean. Cursus ac enim
-								pulvinar curabitur morbi convallis. Lectus malesuada sed fermentum dolore amet.
-							</p>
+							<h2>What our customers say about us...</h2>
 						</header>
-						<p>
-							Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa
-							posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus
-							sit arcu sociis. Nunc fermentum adipiscing tempor cursus nascetur adipiscing adipiscing. Primis aliquam
-							mus lacinia lobortis phasellus suscipit. Fermentum lobortis non tristique ante proin sociis accumsan
-							lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum
-							consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue
-							interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia
-							natoque aenean scelerisque.
-						</p>
-						<footer>
-							<a href="#" class="button">Continue Reading</a>
-						</footer>
+						<div id="testimonials">
+							<div class="blockquote">
+								<h1>
+									<span style="color: #e74848">Rocky Road is my happy place!</span> The staff is so friendly and the food is always delicious. I can't get enough of the homemade cookies."
+								</h1>
+								<h4>&mdash;Sarah L.<br><em>Our new loyal customer</em></h4>
+							</div>
+						</div>
 					</article>
 
 				</div>
