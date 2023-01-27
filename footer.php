7c62fff56baf63f7e82cbbfe7946761d05cb5ea0
Abigail Ponce <abigaiil221b@gmail.com>
2023-01-26 19:37:01 -0800

    Remove extra div end tag

--- footer.php
+++ footer.php
@@ -9,7 +9,9 @@
                 <h3>Location</h3><br>
                 1234 Somewhere St.<br>
                 City, XYZ 123 <br>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4735.276748928483!2d-113.54812176188194!3d53.59991384245045!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1674789358485!5m2!1sen!2sus" width="250" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            <div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4735.276748928483!2d-113.54812176188194!3d53.59991384245045!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1674789358485!5m2!1sen!2sus" width="250" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div>
                 <h3>Opening hours</h3><br>
                 Monday................................6am-9pm<br>
                 Tuesday................................6am-9pm<br>
