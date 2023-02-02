dedf62a14b3cbc61b63e31b980dfaa5097e15d31
Abigail Ponce <abigaiil221b@gmail.com>
2023-02-01 16:48:54 -0800

    Fix footer layout style

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
             <h2>Rocky Road Cafe</h2>
         </header>
         <div class="row">
-            <div>
+            <div class="col-4">
                 <h3>Location</h3><br>
-                1234 Somewhere St.<br>
-                City, XYZ 123 <br>
+                <p>1234 Somewhere St. City, XYZ 123</p>
+                <p>(123) 456-7890</p>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d4735.276748928483!2d-113.54812176188194!3d53.59991384245045!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1674789358485!5m2!1sen!2sus" width="250" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <div>
+            <div class="col-4">
                 <h3>Opening hours</h3><br>
                 Monday................................6am-9pm<br>
                 Tuesday................................6am-9pm<br>
@@ -29,6 +29,13 @@
                 <a href="#">About us</a>
             </div>
         </div>
+        <div class="col-12">
+            <!-- Copyright -->
+                <div class="copyright">
+                    <ul class="menu">
+                        <li>Design based on <a href="https://html5up.net/helios">Helios</a> at <a href="http://html5up.net">HTML5 UP</a> templates.</li>
+                    </ul>
+                </div>
         </div>
     </div>
 </div>
